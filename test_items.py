04c0bfb0c7691290489697b05fd8ec9cf7a4fc45
Andrii Hrohul <grogulflash@gmail.com>
2023-08-22 17:00:49 +0300

    all tasks are done

--- test_items.py
+++ test_items.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.common.by import By
+import time
 
-link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser):
+def test_should_see_add_basket_button(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    time.sleep(10)
+    add_to_basket_button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert len(add_to_basket_button) > 0, "NoSuchElement"
 
-def test_guest_should_see_login_link_fail(browser):
-    browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#magic_link")
+
